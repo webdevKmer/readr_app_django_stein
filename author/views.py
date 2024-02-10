e8866368b83c13593a8c5a23f47c10196df3ce2f
webdevKmer <webdevKmer@gmail.com>
2024-02-10 14:01:19 +0000

    add an author model and create form to add author

--- author/views.py
+++ author/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AuthorForm
+from .models import Author
 
 # Create your views here.
+
+def index(request):
+    authors = Author.objects.all()
+    return render(request, 'author/index.html', {
+        'authors': authors
+    })
+
+def add(request):
+    if request.method == 'POST':
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('author:index')
+    else :
+        form = AuthorForm()
+    return render(request, 'author/index.html', {
+        'form': form
+    })
